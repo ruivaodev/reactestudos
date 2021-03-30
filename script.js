6dc662d240cca5b1d99da2ce9bc6dd9be3e8fc7c
schirmann <cschirmann.dev@gmail.com>
2021-03-30 17:05:30 -0300

    adicionando e removento

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function addBola() {
-  let bola = document.createElement("div");
+  let bola = document.createElement("div"); //criando a div
   bola.setAttribute("class", "bola");
 
   let p1 = Math.floor(Math.random() * 500);
   let p2 = Math.floor(Math.random() * 400);
 
-  bola.setAttribute("style", "left:" + p1 + "px;top:" + p2 + "px;");
-  bola.setAttribute("onclick", "estourar()");
+  bola.setAttribute("style", "left:" + p1 + "px;top:" + p2 + "px;"); // pegando os estilos
+  bola.setAttribute("onclick", "estourar(this)");
 
-  document.body.appendChild(bola);
+  document.body.appendChild(bola); //criando a div
+}
+
+function estourar(elemento) {
+  document.body.removeChild(elemento); //removendo
+}
+function iniciar() {
+  setInterval(addBola, 1000);
 }
