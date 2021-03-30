7aca76cfdec7d45c99d15f3db5227407167a4e02
schirmann <cschirmann.dev@gmail.com>
2021-03-30 16:55:59 -0300

    adicionando a tela

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function addBola() {
   let bola = document.createElement("div");
   bola.setAttribute("class", "bola");
+
+  let p1 = Math.floor(Math.random() * 500);
+  let p2 = Math.floor(Math.random() * 400);
+
+  bola.setAttribute("style", "left:" + p1 + "px;top:" + p2 + "px;");
+  bola.setAttribute("onclick", "estourar()");
+
+  document.body.appendChild(bola);
 }
